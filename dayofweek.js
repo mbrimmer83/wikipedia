7f1e9937693cd1bb689c4b6a996f8cffbb27adfb
Matthew <mbrimmer1@gmail.com>
2016-05-04 17:05:21 -0400

    Change

---
+++ dayofweek.js
@@ -0,0 +1,17 @@
+var day = 0;
+
+if (day === 0) {
+  console.log("Sunday");
+} else if (day === 1) {
+  console.log("Monday");
+} else if (day === 2) {
+  console.log("Tueday");
+} else if (day === 3) {
+  console.log("wednesday");
+} else if (day === 4) {
+  console.log("Thursday");
+} else if (day === 5) {
+  console.log("Friday");
+}  else if (day === 6) {
+  console.log("Saturday")
+}
